8629fb1f597083e8754fbb17a2dda544dcb6c523
sajaalmuraqtn <sajaalmuraqtn@gmail.com>
2025-09-13 18:00:10 +0300

    edit in design

--- lib/helpers/data_center.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:quiz_std/helpers/color_helper.dart';
-import 'package:quiz_std/logic/level.dart';
-
-List <Level> levels=[
-  Level(title: "True & False", subtitle: "level 1", image: 'bags.png', description: "jsdjfi", icon: Icons.check, colors: [kL1,kL12])
-  
-
-];
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_std/screens/home.dart';
-import 'package:quiz_std/screens/multiple_quiz.dart';
+ import 'package:quiz_std/screens/multiple_quiz.dart';
 import 'package:quiz_std/screens/true_false_quiz.dart';
 
 void main() {
   runApp( MaterialApp(
-    home: HomeScreen(),
+    debugShowCheckedModeBanner: false,
+    home: TrueFalseApp(),
   ));
 }
 
